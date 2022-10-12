f1d8166f7dbcf29ca6872232922411cd66d8686f
Dippudo <107745404+dippudo@users.noreply.github.com>
2022-10-12 19:21:08 +1100

    Update InvestorVideoGame.py

--- InvestorVideoGame.py
+++ InvestorVideoGame.py
@@ -4,7 +4,7 @@ from sys import platform
 
 ########## DEFINING FUNCTIONS ##########
 
-##### Check user's operating system and clears the terminal
+# CLEAR USER TERMINAL AFTER CHECKING THEIR OPERATING SYSTEM
 def clear_terminal():
     if platform == "linux" or platform == "linux2": # LINUX
         os.system("clear")
@@ -15,24 +15,67 @@ def clear_terminal():
     elif platform == "win32": # WINDOWS
         os.system("cls")
 
-clear_terminal()
+    else:
+        os.system("cls")
 
 
-##### Prints a new line, to have all printed text one line under the top of the terminal window
+# PRINT A NEW LINE
 def empty_line():
     print("\n")
 
 
-#####
+# NEW WINDOW: CLEAR TERMINAL + ADD NEW LINE
 def start_new_window():
     clear_terminal()
     empty_line()
 
 
+# DISPLAY THE RULES OF THE GAME
+def display_rules():
+    print("+-------------------------------------------------------------------------  ABOUT THE GAME  --------------------------------------------------------------------------+")
+    print("|                                                                                                                                                                     |")
+    print("|    This game is an investing simulation that enables players to learn the risks and rewards involved in the decision-making and risk-taking of financial markets.   |")
+    print("|                                                                                                                                                                     |")
+    print("|    You will start off with a balance of $100,000 to invest and grow your money through your chosen asset(s).                                                        |")
+    print("|    Your goal is to win the challenge by growing your money into $1,000,000.                                                                                         |")
+    print("|                                                                                                                                                                     |")
+    print("|    A fully randomised set of economic events will be presented to you, and you will be given the option to buy, hold, or sell.                                      |")
+    print("|    In order to win the game, you must search for clues in the news, read between the lines, and play some hunches.                                                  |")
+    print("|                                                                                                                                                                     |")
+    print("|    The game will end once you have beat the challenge, or run out of money.                                                                                         |")
+    print("|                                                                                                                                                                     |")
+    print("+---------------------------------------------------------------------------------------------------------------------------------------------------------------------+")
+
+
+# PRESS ANY KEY TO CONTINUE OR TYPE "QUIT" TO QUIT
+def continue_or_quit():
+    CorQ = input("Press any key to continue, or type 'QUIT' to quit the game.")
+    CorQ_lower = CorQ.lower()
+
+    if CorQ_lower == "quit":
+        print("Come back next time when you are ready, see you soon.")
+        quit()
+    
+    time.sleep(1)
+
+
+# DISPLAY INVESTMENT OPTIONS
+def display_ivestment_options():
+    print("+--------  INVESTMENT OPTIONS  --------+")
+    print("|                                      |")
+    print("+--  Number  ---  Class  --------------+")
+    print("|    1            Stocks               |")
+    print("|    2            Real estate          |")
+    print("|    3            Lottery tickets      |")
+    print("|    4            Cryptocurrencies     |")
+    print("|                                      |")
+    print("+--------------------------------------+")
+
+    
+
 ########## INTRO ##########
 
 clear_terminal()
-empty_line()
 
 username = input("Hello there! Please enter your desired username: ")
 
@@ -51,20 +94,32 @@ Hello, {username}, and welcome to:
 """)
 
 ready_for_rules = input("Are you ready to review the rules? (yes/no): ")
-
 ready_for_rules_lower = ready_for_rules.lower()
 
-if ready_for_rules_lower == "yes":
-    print("Let's begin")
 
-else:
+print(ready_for_rules_lower)
+time.sleep(1)
+
+if ready_for_rules_lower != "yes":
     print("You are inexperienced and will get rekt.")
     print("Come back next time when you are ready, see you soon.")
     quit()
 
+print("Let's begin")
+time.sleep(1)
 
-##### Dislay rules
+
+########## DISPLAY RULES ##########
 
 clear_terminal()
 
-print("The goal of this game is to make money.")
+display_rules()
+
+empty_line()
+continue_or_quit()
+
+empty_line()
+display_ivestment_options()
+
+empty_line()
+continue_or_quit()
